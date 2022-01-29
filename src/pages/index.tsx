5dfa07577c0e9798a2bcbac7cb37add2d39589ab
Jucemar Dimon <jucemar.dimon@gmail.com>
2022-01-29 20:15:13 -0300

    Adiciona componente Head para incluir titulos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import styles from '../styles/home.module.scss';
+import styles from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (  
+    <>
+    <Head>
+    <title>Início | JDNEWS</title>
+    </Head>
+
     <h1 className={styles.title}>
       Hello Word!
       </h1>
+      </>
   )
 }
