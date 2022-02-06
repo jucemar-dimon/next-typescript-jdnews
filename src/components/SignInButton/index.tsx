494a357fec18877a7dd771bc2ace61f7e94195d9
Jucemar Dimon <jucemar.dimon@gmail.com>
2022-02-06 16:12:16 -0300

    Adiciona a estrutura para autenticação com github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
-  return isUserLoggedIn ? (
+  const { data: session } = useSession();
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Sign in width Github
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
-      Jucemar Dimon
-      <FiX color="#737380" className={styles.closeIcon} />
+      Sign in width Github
     </button>
   );
 }
